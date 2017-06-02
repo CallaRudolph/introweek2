add66d8dc3cf0447d1cf8a6cbf31ce5e96c3b107
Calla Rudolph <callarudolph@gmail.com>
2017-06-02 14:05:43 -0700

    change #track to #css to make multiple output sections

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,28 @@ $(document).ready(function() {
 
     var nameInput = $("input#name").val();
     var localeInput = $("#location").val();
-
-    var java;
-    if (apps === 3 && end === 1) {
-      java = ("test");
-    }
-    var ruby;
-    if (apps === 4 && end === 2 && job === 5) {
-      ruby = ("TEST");
-    }
-
-
+    
     $(".name").text(nameInput);
     $(".location").text(localeInput);
-    $("#course").empty().text(java);
-    $("#course").empty().text(ruby);
-    $("#track").show();
+
+    if (end === 2 && job === 6) {
+      $("#css").show();
+    }
+    // var css;
+    // if (end === 2 && job === 6) {
+    //   css = ("CSS");
+    // }
+    // // var ruby;
+    // //   if (apps === 4 && end === 2 && job === 5) {
+    // //     ruby = ("TEST");
+    // // }
+    // var java;
+    // if (apps === 3 && end === 1) {
+    //   java = ("Java/Android");
+    // }
+
+
+
 
 
   });
