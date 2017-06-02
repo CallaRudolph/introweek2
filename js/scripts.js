39131efecbd0ba002cf7ff9419d30ea407dac241
Calla Rudolph <callarudolph@gmail.com>
2017-06-02 13:03:09 -0700

    create end & css variables with if statement to display on #track

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $("#formTwo").submit(function(event) {
+    var end = parseInt($("#end").val());
     var nameInput = $("input#name").val();
-    var locale = $("#location").val();
+    var localeInput = $("#location").val();
+    var css = ("CSS/Design");
+    if (end === 1)
 
     $(".name").text(nameInput);
-    $(".location").text(locale);
+    $(".location").text(localeInput);
+
+    $("#course").empty().append(css);
     $("#track").show();
 
   event.preventDefault();
