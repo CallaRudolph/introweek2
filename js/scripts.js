89610f24627f3a6b7dc66e9844efd83daee8ee54
Calla Rudolph <calla@>
2017-06-06 12:43:57 -0700

    update .hide for each id in js

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,27 @@ $(document).ready(function() {
 
     if (end === 2 && apps === 3 && job === 6) {
       $("#css").show();
+      $("#java").hide();
+      $("#ruby").hide();
+      $("#net").hide();
     }
     if (end === 1 && apps === 3) {
       $("#java").show();
+      $("#css").hide();
+      $("#ruby").hide();
+      $("#net").hide();
     }
     if (end === 2 && apps === 4 && job === 5) {
       $("#ruby").show();
+      $("#java").hide();
+      $("#css").hide();
+      $("#net").hide();
     }
     if (end === 2 && apps === 4 && job === 6) {
       $("#net").show();
+      $("#java").hide();
+      $("#ruby").hide();
+      $("#css").hide();
     }
   });
 });
